80b49ab068c04f4a27f6400d1e4c89e639ffcb5f
kvitorr <kvitorrsantos@gmail.com>
2023-03-08 21:00:21 -0300

    att

--- classe/BattleShipGame.ts
+++ classe/BattleShipGame.ts
@@ -9,31 +9,54 @@ export class BattleShipGame{
     private _turn: User;
 
     constructor(player1: User, player2: User, positionBoats1: string, positionBoats2: string, turn: User) {
-        this._player1= player1;
-        this._player2= player2;
+        this._player1 = player1;
+        this._player2 = player2;
         this._board1 =  new Board(positionBoats1);
         this._board2 = new Board(positionBoats2);
         this._turn = turn;   
     }
 
-
-
-    private attackOpponent = (currentPlayer: User, position: string) => {
-        if(currentPlayer.nickname == this._player1.nickname){
+    public attackOpponent = (position: string) => {
+        if(this._turn.nickname == this._player1.nickname){
             this._board2.receberAttack(position)
+            this.showBoardsOff()
+            this._turn = this._player2
         } else {
             this._board1.receberAttack(position)
+            this.showBoardsOff()
+            this._turn = this._player1
         }
     }
 
-    public showBoards = (currentPlayer: User) =>{
-        if(currentPlayer.nickname == this._player1.nickname){
-            this._board1.printTabuleiroCurrentPlayer()
+    public showBoards = () => {
+
+        if(this._turn.nickname == this._player1.nickname){
+            this._turn.socket.write(`PrintPlayer ${this._board1.printTabuleiroCurrentPlayer()}`)
+            this._turn.socket.write(`PrintOpponent ${this._board1.printTabuleiroOpponent()}`)    
         } else {
-            this._board1.receberAttack
+            this._turn.socket.write(`PrintPlayer ${this._board2.printTabuleiroCurrentPlayer()}`)
+            this._turn.socket.write(`PrintOpponent ${this._board2.printTabuleiroOpponent()}`)
+        }
+
+
+    }
+
+    private showBoardsOff = () => {
+        if(this._turn.nickname == this._player1.nickname){
+            this._turn.socket.write(`FinalPrint ${this._board1.printTabuleiroCurrentPlayer()}`)
+            this._turn.socket.write(`FinalPrint ${this._board1.printTabuleiroOpponent()}`) 
+        } else {
+            this._turn.socket.write(`FinalPrint ${this._board2.printTabuleiroCurrentPlayer()}`)
+            this._turn.socket.write(`FinalPrint ${this._board2.printTabuleiroOpponent()}`) 
         }
     }
 
+    public verificarFimDaPartida = () => {
+        if(this._turn.nickname == this._player1.nickname){
 
+        } else {
+
+        }
+    }
     
 }
